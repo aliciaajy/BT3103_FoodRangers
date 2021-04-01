28cedba55da6f74dc5769c56fbb40e88e2cb101d
AngelicaTay <angelicatay3@gmail.com>
2021-04-01 18:27:55 +0800

    fixed favouriting issues not showing

--- src/components/search/favButton.vue
+++ src/components/search/favButton.vue
@@ -1,7 +1,7 @@
 <template>
 
 
-  <button id="like" v-on:click="favMart(); heartit()" :class="['heart-btn', {liked: this.likeToggle}]"> 
+  <button id="like" v-on:click="favMart();" :class="['heart-btn', {liked: this.likeToggle}]"> 
      <svg class="heart heart-icon" viewBox="0 0 32 29.6">
     <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
     c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
@@ -44,7 +44,7 @@
 
 
             //var lst=[];
-            alert("mart id " + this.mart[0]);
+           // alert("mart id " + this.mart[0]);
             alert("added")
           db.collection('favMart').doc(added[0]).set(added[1]);
             //alert(this.itemsSelected + " saved to database");
@@ -68,7 +68,7 @@
 
         deleteMart: function() {
           let doc_id = this.mart[0];
-          alert("doc id is " + doc_id);
+          //alert("doc id is " + doc_id);
           alert("deleted");
           db.collection('favMart').doc(doc_id).delete();
         },
@@ -84,27 +84,31 @@
           }
         },
 
-        heartit: function (e) {
-      const hearts = document.createElement('div');
-      hearts.innerHTML = '<svg class="heart heart-pop one" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/></svg><svg class="heart heart-pop two" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/></svg><svg class="heart heart-pop three" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/></svg><svg class="heart heart-pop four" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/></svg><svg class="heart heart-pop five" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/></svg><svg class="heart heart-pop six" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/></svg><svg class="heart heart-pop seven" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/></svg><svg class="heart heart-pop eight" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/></svg><svg class="heart heart-pop nine" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/></svg>';
-      e.target.appendChild(hearts);
-      //this.liked = !this.liked;
-      setTimeout(function(){
-        e.target.removeChild(hearts);
-      }, 3000);
-    }
+        displayFav: function() {
+
+          let doc_id = this.mart[0];
+          var ref = db.collection("favMart").doc(doc_id);
+
+          ref.get().then((doc => {
+            if (doc.exists) {
+              //if the doc exists = mart was previously favourited
+              //set liketoggle to true
+              this.setToggle();
+            }
+          }))
+        }
+
+
 
         //methods end here
       },
 
-       mounted(){
-  document.body.addEventListener('mousedown', function() {
-    document.body.classList.add('using-mouse');
-  });
-  document.body.addEventListener('keydown', function() {
-    document.body.classList.remove('using-mouse');
-  });
- }
+      created() {
+        this.displayFav();
+
+      }
+
+
     }
 
 
