345dc88178ee8314dfaa64d4466ff7790cb9d3b6
aliciaajy <65158683+aliciaajy@users.noreply.github.com>
2021-04-07 01:20:07 +0800

    Revert "Merge branch 'main' of https://github.com/aliciaajy/BT3103_FoodRangers into main"
    This reverts commit be821202be736b8bc54647a384ee950d76b3cb7f, reversing
    changes made to 6e0bc016615a1d02331de84981d6f81f1932a681.

--- src/components/favourites/favourites.vue
+++ src/components/favourites/favourites.vue
@@ -47,7 +47,6 @@
 import db from "../../firebase.js";
 import starRatings from "../search/starRatings.vue";
 import like from "./favButton.vue";
-import firebase from "firebase";
 export default {
   components: {
     starRatings,
@@ -65,7 +64,7 @@ export default {
    methods: {
 
     fetchItems:function(){ 
-           db.collection('favMart').where("userid","==",firebase.auth().currentUser.uid).orderBy('name').get().then((querySnapShot)=>{
+           db.collection('favMart').orderBy('name').get().then((querySnapShot)=>{
                let mart={} 
                querySnapShot.forEach(doc=>{
                     mart=[doc.id,doc.data()]
