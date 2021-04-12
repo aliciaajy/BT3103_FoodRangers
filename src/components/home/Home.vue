9cccca4e195b68574eb3dadb242a108b9926aa56
aliciaajy <65158683+aliciaajy@users.noreply.github.com>
2021-04-12 16:45:32 +0800

    did up the notes and annoucements

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,22 +3,48 @@
     <Bar></Bar>
     
     <h1>
-      <br>
+      
       Welcome Back,
       <small class="text-muted">Name</small>
+      <h2 id ="annword">  Annoucement </h2>
     </h1>
+    <Notes></Notes>
+    <Annoucement></Annoucement>
   </div>
+
 </template>
 
 
 
 
 <script>
-export default {};
+
+import Annoucement from './Annoucement.vue';
+import Notes from './Notes.vue';
+
+export default {
+  components: {
+    Annoucement,
+    Notes
+  }
+};
+
 </script>
 
 <style scoped>
 h1 {
   text-align:left;
+}
+h3 {
+  right: 20px;
+}
+#annword {
+    float:right;
+    width:35%;
+    height: 40px;
+    background: rgba(10, 11, 94, 0.603);
+    color: white;
+    border: 1.8px solid rgb(3, 3, 3);
+    text-align: center;
 }
 </style>
